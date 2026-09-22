7e6865800df4477b10ceef0531357fdc9e33b6e2
Abhay02092005 <namdevabhay2005@gmail.com>
2026-09-22 16:07:36 +0530

    Add files via upload

---
+++ abhayn.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int P_sal,C_sal;
+cout<<"Enter previous salary";
+cin>>P_sal;
+cout<<"Enter current salary";
+cin>>C_sal;
+int hike;
+hike = C_sal-P_sal;
+cout<<"hike="<<hike;
+return 0;
+
+}
